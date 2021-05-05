5e8bc3be29fab4c1bd8cb9ab9759f655e700b06f
JosepLeder <1225893435@qq.com>
2021-05-05 17:54:08 +0800

    update scripts

--- scripts/run_sawyer_pickandplace.sh
+++ scripts/run_sawyer_pickandplace.sh
@@ -1,6 +1,5 @@
-
-python tools/run_env.py --env RepeatGraspEnv \
-                        --policy RepeatedRandomGraspPolicy \
-                        --env_config configs/envs/push_env.yaml \
-                        --policy_config configs/policies/heuristic_push_policy.yaml
-                        --debug 1 
+python tools/run_env.py --env RepeatGraspEnv\
+	--policy RepeatedRandomGraspPolicy 
+	--env_config configs/envs/repeat_grasp_env.yaml\ 
+	--policy_config configs/policies/repeated_random_grasp_policy.yaml\
+	--debug 1
